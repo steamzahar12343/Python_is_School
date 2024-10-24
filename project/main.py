ae625a9322abfa5c685c4cf23e91cb5c5c3b0210
Mister <124846656+steamzahar12343@users.noreply.github.com>
2024-10-24 14:18:19 +0300

    12

--- project/main.py
+++ project/main.py
@@ -1,32 +1,12 @@
-lenBoard = int(input())
-lenGub = int(input())
+import turtle
 
-LENGTH = 0
+t = turtle
+t.screensize(500,500)
+t.speed(10)
 
-stroke = ""
+def cube(dista: int):
+    for i in range(4):
+        t.forward(dis)        
 
-SBoard = lenBoard * lenBoard
-SGub = lenGub * lenGub
 
-CountTiles = int(SBoard / SGub)
-
-ColorTiles = []
-
-for x in range(lenBoard):
-    for y in range(lenBoard):
-        temp = False        
-        for i in ColorTiles:
-            if(i == [[x,y]]):
-                temp = True
-                break
-            else:
-                temp = False
-                break
-            
-        if(temp):
-            continue
-        else:
-            ColorTiles += [[x,y]]
-            LENGTH += 1
-
-print(LENGTH)
+t.exitonclick()
