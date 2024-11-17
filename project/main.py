44ae8dd0c6bdcfa9f34bbdd32ca8d09d5d4e39d4
steamzahar12343 <evgenyaplusha23@gmail.com>
2024-11-17 03:08:24 +0300

    Update calculator

--- project/main.py
+++ project/main.py
@@ -1,39 +1,68 @@
-from tkinter import *
-from tkinter import ttk
-
-root = Tk()
+import tkinter as ttk
+            #Настройка
+root = ttk.Tk()
 root.title("Calculator")
-root.geometry("200x100")
+root.geometry("400x200")
 root.resizable(False, False)
-
+root.configure(bg="#b0b8b5")
+            #Логика кнопок
 def commandPlus():
-    pass
+    try: 
+        LblInfo["text"] = str(int(entryNumb1.get()) + int(entryNumb2.get()))
+    except:
+        LblInfo["text"] = "e"
+        
+def commandMinus():
+    try: 
+        LblInfo["text"] = str(int(entryNumb1.get()) - int(entryNumb2.get()))
+    except:
+        LblInfo["text"] = "e"
 
-entryNumb1 = ttk.Entry()
-entryNumb2 = ttk.Entry()
-btnPlus = ttk.Button(text = "+", command=commandPlus)
-btnMinus = ttk.Button(text = "-")
-btnDevide = ttk.Button(text = "/")
-btnMultiply = ttk.Button(text = "*")
-#btnResult = ttk.Button(text = "=", width=20)
-LblInfo = ttk.Label(text = "test//")
+def commandMultiply():
+    try: 
+        LblInfo["text"] = str(int(entryNumb1.get()) * int(entryNumb2.get()))
+    except:
+        LblInfo["text"] = "e"
+    
+def commandDevide():
+    try: 
+        LblInfo["text"] = str(int(entryNumb1.get()) / int(entryNumb2.get()))
+    except:
+        LblInfo["text"] = "e"
+        
+def commandCE():
+    LblInfo["text"] = 0
+    entryNumb1.delete(0, 100)
+    entryNumb2.delete(0, 100)
+            #Инициализация входных данных      
+entryNumb1 = ttk.Entry(relief="ridge", highlightbackground="red", highlightcolor="red", font=("Arial", 24))
+entryNumb2 = ttk.Entry(relief="ridge", highlightbackground="red", highlightcolor="red", font=("Arial", 24))
+            #Инициализация кнопок
+btnPlus = ttk.Button(text = "+", command=commandPlus, borderwidth=2)
+btnMinus = ttk.Button(text = "-", command=commandMinus, borderwidth=2)
+btnMultiply = ttk.Button(text = "*", command=commandMultiply, borderwidth=2)
+btnDevide = ttk.Button(text = "/", command=commandDevide, borderwidth=2)
+btnCE = ttk.Button(text = "CE", command=commandCE)
+ALL = [btnPlus, btnMinus, btnMultiply, btnDevide, btnCE]
+            #Инициализация ввода
+LblInfo = ttk.Label(text = "0", font=("Arial", 25), justify = "center", background="#b0b8b5")
+#Вывод на экран:
+for i in ALL:
+    i.config(width = 10, height = 2)
 
-entryNumb1.grid(row=0,column=0)
-entryNumb2.grid(row=1,column=0)
-btnPlus.grid(row=0,column=1)
-btnMinus.grid(row=1,column=1)
-btnDevide.grid(row=2,column=1)
-btnMultiply.grid(row=3,column=1)
-#btnResult.grid(row=2,column=0)
-LblInfo.grid(row=3,column=0)
+for c in range(3): root.columnconfigure(index = c , weight = 1)
+for r in range(3): root.rowconfigure(index = r, weight = 1)
 
-#entryNumb1.pack(anchor="nw")
-#entryNumb2.pack(anchor="nw")
+entryNumb1.grid(row = 1, column = 1)
+entryNumb2.grid(row = 2, column = 1)
 
-#btnPlus.pack()
-#btnMinus.pack()
-#btnDevide.pack()
-#btnMultiply.pack() 
-#btnResult.pack()
-#LblInfo.pack()
+btnPlus.grid(row = 1, column = 0)
+btnMinus.grid(row = 2, column = 0)
+btnMultiply.grid(row = 1, column = 2)
+btnDevide.grid(row = 2, column = 2)
+btnCE.grid(row = 0, column = 2)
+
+LblInfo.grid(row = 0, column = 1)
+
+
 root.mainloop()
